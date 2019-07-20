5811004dba8bfa452fe6b95e514e2ba42fa1e849
Saga Kusuma Wijaya <45896812+sagakusumawijaya@users.noreply.github.com>
2019-07-21 05:48:12 +0700

    Add files via upload

---
+++ BAB 7 - Mengendalikan Motor/Mengendalikan Motor Servo/Bab_7_Code_MotorServo_Test_Sweep.ino
@@ -0,0 +1,45 @@
+﻿/*
+ * Author         : Saga Kusuma Wijaya
+ * Date Created   : 20 July 2019
+ * Project        : Tutorial Arduino ATM
+ * Dedicated to   : emtronics
+ * Youtube        : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
+ * Instagram      : https://www.instagram.com/emtronics.official/
+ * Website        : https://emtronicsofficial.com
+ * Facebook		  : https://www.facebook.com/emtronics.official/
+ * Email          : contact@emtronicsofficial.com
+ *
+ * Description    : This is an example .ino file of Servo.h librarie
+ */
+
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+	myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+	for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+	  // in steps of 1 degree
+		myservo.write(pos);              // tell servo to go to position in variable 'pos'
+		delay(15);                       // waits 15ms for the servo to reach the position
+	}
+	for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+		myservo.write(pos);              // tell servo to go to position in variable 'pos'
+		delay(15);                       // waits 15ms for the servo to reach the position
+	}
+}
