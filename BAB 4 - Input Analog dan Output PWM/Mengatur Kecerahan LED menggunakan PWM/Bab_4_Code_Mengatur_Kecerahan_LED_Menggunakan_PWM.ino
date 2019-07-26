d0f9fc6929364087a1d11390c376010139c971f4
Saga Kusuma Wijaya <45896812+sagakusumawijaya@users.noreply.github.com>
2019-07-27 02:20:43 +0700

    Create Bab_4_Code_Mengatur_Kecerahan_LED_Menggunakan_PWM.ino

---
+++ BAB 4 - Input Analog dan Output PWM/Mengatur Kecerahan LED menggunakan PWM/Bab_4_Code_Mengatur_Kecerahan_LED_Menggunakan_PWM.ino
@@ -0,0 +1,35 @@
+/*
+ * Author         : Saga Kusuma Wijaya
+ * Date Created   : 9 May 2019
+ * Project        : Tutorial Arduino ATM
+ * Dedicated to   : emtronics
+ * Youtube        : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
+ * Instagram      : https://www.instagram.com/emtronics.official/
+ * Website        : https://emtronicsofficial.com
+ * Facebook		  : https://www.facebook.com/emtronics.official/
+ * Email          : contact@emtronicsofficial.com
+ *
+ * Description    :
+ */
+
+#define pwmLED 6
+
+void setup() {
+	pinMode(pwmLED, OUTPUT);
+}
+
+void loop() {
+	// for-loop digunakan untuk meningkatkan kecerahan led
+	for (int i = 0; i <= 255; i += 5) {
+		analogWrite(pwmLED, i);
+		delay(50);
+	}
+	delay(1000);  // tunggu 1 detik ketika led berada di titik paling terang
+
+	// for-loop digunakan untuk menurunkan kecerahan led
+	for (int i = 255; i >= 0; i -= 5) {
+		analogWrite(pwmLED, i);
+		delay(50);
+	}
+	delay(1000);  // tunggu 1 detik ketika led berada di titik paling redup (led padam(
+}
