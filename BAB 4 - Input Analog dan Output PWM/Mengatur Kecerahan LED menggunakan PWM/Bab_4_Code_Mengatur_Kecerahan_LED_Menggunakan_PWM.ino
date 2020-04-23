0ade4d9414190a92afeeabf55eea8ea06f218a39
Saga Kusuma Wijaya <45896812+sagakusumawijaya@users.noreply.github.com>
2020-04-24 02:15:59 +0700

    Update Bab_4_Code_Mengatur_Kecerahan_LED_Menggunakan_PWM.ino

--- BAB 4 - Input Analog dan Output PWM/Mengatur Kecerahan LED menggunakan PWM/Bab_4_Code_Mengatur_Kecerahan_LED_Menggunakan_PWM.ino
+++ BAB 4 - Input Analog dan Output PWM/Mengatur Kecerahan LED menggunakan PWM/Bab_4_Code_Mengatur_Kecerahan_LED_Menggunakan_PWM.ino
@@ -1,16 +1,15 @@
-/*
- * Author         : Saga Kusuma Wijaya
- * Date Created   : 9 May 2019
- * Project        : Tutorial Arduino ATM
- * Dedicated to   : emtronics
- * Youtube        : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
- * Instagram      : https://www.instagram.com/emtronics.official/
- * Website        : https://emtronicsofficial.com
- * Facebook		  : https://www.facebook.com/emtronics.official/
- * Email          : contact@emtronicsofficial.com
- *
- * Description    :
- */
+/**
+ * Author       : Saga Kusuma Wijaya
+ * Date Created : 9 May 2019
+ * Project      : Tutorial Arduino ATM
+ * Dedicated to : emtronics
+ * Youtube      : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
+ * Instagram    : https://www.instagram.com/emtronics.official/
+ * Facebook     : https://web.facebook.com/emtronics.official/
+ * Website      : https://sagakusumawijaya94.wordpress.com/tutorials/arduino-tutorial/
+ * Github       : https://github.com/sagakusumawijaya
+ * Email        : emtronics.2018@gmail.com
+ **/
 
 #define pwmLED 6
 
