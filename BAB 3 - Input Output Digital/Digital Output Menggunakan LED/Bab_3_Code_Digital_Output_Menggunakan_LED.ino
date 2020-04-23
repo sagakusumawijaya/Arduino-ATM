cb475e31bcb334b3fb53d82e92fab3f7029b8c1c
Saga Kusuma Wijaya <45896812+sagakusumawijaya@users.noreply.github.com>
2020-04-24 02:11:54 +0700

    Update Bab_3_Code_Digital_Output_Menggunakan_LED.ino

--- BAB 3 - Input Output Digital/Digital Output Menggunakan LED/Bab_3_Code_Digital_Output_Menggunakan_LED.ino
+++ BAB 3 - Input Output Digital/Digital Output Menggunakan LED/Bab_3_Code_Digital_Output_Menggunakan_LED.ino
@@ -1,16 +1,15 @@
-/*
- * Author         : Saga Kusuma Wijaya
- * Date Created   : 8 May 2019
- * Project        : Tutorial Arduino ATM
- * Dedicated to   : emtronics
- * Youtube        : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
- * Instagram      : https://www.instagram.com/emtronics.official/
- * Website        : https://emtronicsofficial.com
- * Facebook	      : https://www.facebook.com/emtronics.official/
- * Email          : contact@emtronicsofficial.com
- *
- * Description    : Demonstrates how to use LED as output digital with Arduino.
- */
+/**
+ * Author       : Saga Kusuma Wijaya
+ * Date Created : 8 May 2019
+ * Project      : Tutorial Arduino ATM
+ * Dedicated to : emtronics
+ * Youtube      : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
+ * Instagram    : https://www.instagram.com/emtronics.official/
+ * Facebook     : https://web.facebook.com/emtronics.official/
+ * Website      : https://sagakusumawijaya94.wordpress.com/2020/04/21/output-digital-menggunakan-led/
+ * Github       : https://github.com/sagakusumawijaya
+ * Email        : emtronics.2018@gmail.com
+ **/
 
 #define pinLED 5
 
