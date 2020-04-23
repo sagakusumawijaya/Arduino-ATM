99b80e1a45cc5508fd8ed37d536067284cce601c
Saga Kusuma Wijaya <45896812+sagakusumawijaya@users.noreply.github.com>
2020-04-24 02:35:04 +0700

    Update Bab_7_Code_Motor_Vibration_Test.ino

--- BAB 7 - Mengendalikan Motor/Mengendalikan Motor Vibration/Bab_7_Code_Motor_Vibration_Test.ino
+++ BAB 7 - Mengendalikan Motor/Mengendalikan Motor Vibration/Bab_7_Code_Motor_Vibration_Test.ino
@@ -1,16 +1,15 @@
-/*
- * Author         : Saga Kusuma Wijaya
- * Date Created   : 20 July 2019
- * Project        : Tutorial Arduino ATM
- * Dedicated to   : emtronics
- * Youtube        : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
- * Instagram      : https://www.instagram.com/emtronics.official/
- * Website        : https://emtronicsofficial.com
- * Facebook       : https://www.facebook.com/emtronics.official/
- * Email          : contact@emtronicsofficial.com
- *
- * Description    : Enable motor vibration for 1 second and disable for 0.5 second repeatedly.
- */
+/**
+ * Author       : Saga Kusuma Wijaya
+ * Date Created : 20 July 2019
+ * Project      : Tutorial Arduino ATM
+ * Dedicated to : emtronics
+ * Youtube      : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
+ * Instagram    : https://www.instagram.com/emtronics.official/
+ * Facebook     : https://web.facebook.com/emtronics.official/
+ * Website      : ---
+ * Github       : https://github.com/sagakusumawijaya
+ * Email        : emtronics.2018@gmail.com
+ **/
 
 const int motorPin = 2;
 
