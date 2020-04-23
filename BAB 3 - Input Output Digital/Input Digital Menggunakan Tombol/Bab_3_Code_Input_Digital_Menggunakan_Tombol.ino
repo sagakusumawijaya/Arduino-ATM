ced024c0347cff877e387a9c7be1d3eecb9c5f74
Saga Kusuma Wijaya <45896812+sagakusumawijaya@users.noreply.github.com>
2020-04-24 02:04:37 +0700

    Update Bab_3_Code_Input_Digital_Menggunakan_Tombol.ino

--- BAB 3 - Input Output Digital/Input Digital Menggunakan Tombol/Bab_3_Code_Input_Digital_Menggunakan_Tombol.ino
+++ BAB 3 - Input Output Digital/Input Digital Menggunakan Tombol/Bab_3_Code_Input_Digital_Menggunakan_Tombol.ino
@@ -1,16 +1,15 @@
-/*
- * Author         : Saga Kusuma Wijaya
- * Date Created   : 8 May 2019
- * Project        : Tutorial Arduino ATM
- * Dedicated to   : emtronics
- * Youtube        : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
- * Instagram      : https://www.instagram.com/emtronics.official/
- * Website        : https://emtronicsofficial.com
- * Facebook	  : https://www.facebook.com/emtronics.official/
- * Email          : contact@emtronicsofficial.com
- *
- * Description    : Demonstration how to use pushbutton with Arduino.
- */
+/**
+ * Author       : Saga Kusuma Wijaya
+ * Date Created : 8 May 2019
+ * Project      : Tutorial Arduino ATM
+ * Dedicated to : emtronics
+ * Youtube      : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
+ * Instagram    : https://www.instagram.com/emtronics.official/
+ * Facebook     : https://web.facebook.com/emtronics.official/
+ * Website      : ---
+ * Github       : https://github.com/sagakusumawijaya
+ * Email        : emtronics.2018@gmail.com
+ **/
 
 #define tombol1 4
 #define tombol2 6
