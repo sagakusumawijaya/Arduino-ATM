1811bd7cc743093ae32fdc53231eff775c474ee8
Saga Kusuma Wijaya <45896812+sagakusumawijaya@users.noreply.github.com>
2020-04-24 23:13:38 +0700

    Update Bab_3_Code_Project_Menyalakan_LED_Menggunakan_Tombol.ino

--- BAB 3 - Input Output Digital/Project Menyalakan LED Menggunakan Tombol/Bab_3_Code_Project_Menyalakan_LED_Menggunakan_Tombol.ino
+++ BAB 3 - Input Output Digital/Project Menyalakan LED Menggunakan Tombol/Bab_3_Code_Project_Menyalakan_LED_Menggunakan_Tombol.ino
@@ -1,27 +1,43 @@
-/*
- * Author         : Saga Kusuma Wijaya
- * Date Created   : 9 May 2019
- * Project        : Tutorial Arduino ATM
- * Dedicated to   : emtronics
- * Youtube        : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
- * Instagram      : https://www.instagram.com/emtronics.official/
- * Website        : https://emtronicsofficial.com
- * Facebook		    : https://www.facebook.com/emtronics.official/
- * Email          : contact@emtronicsofficial.com
- *
- * Description    : Demonstrates how to use pushbutton to activate LED with Arduino
- */
+/**
+ * Author       : Saga Kusuma Wijaya
+ * Date Created : .....
+ * Project      : Tutorial Arduino ATM
+ * Description  : BAB 3 - Input Output Digital
+ * Dedicated to : emtronics
+ * Youtube      : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
+ * Instagram    : https://www.instagram.com/emtronics.official/
+ * Facebook     : https://web.facebook.com/emtronics.official/
+ * Website      : https://sagakusumawijaya94.wordpress.com/tutorials/arduino-tutorial/
+ * Github       : https://github.com/sagakusumawijaya
+ * Email        : emtronics.2018@gmail.com
+ **/
 
-#define led 6
-#define tombol 2
+#define pinLed 6
+#define pinTombol 2
 
 bool kondisiTombol = false;
+bool kondisiTombolSebelumnya = false;
+bool kondisiLed = false;
+
 void setup() {
-	pinMode(led, OUTPUT);
-	pinMode(tombol, INPUT_PULLUP);
+  pinMode(pinLed, OUTPUT);
+  pinMode(pinTombol, INPUT_PULLUP);  // tombol diset dengan eksternal pull-up resistor
 }
 
 void loop() {
-	kondisiTombol = !digitalRead(tombol);
-	digitalWrite(led, kondisiTombol);
+  kondisiTombol = digitalRead(pinTombol);
+  if (kondisiTombol == false && kondisiTombolSebelumnya != kondisiTombol) {
+    if (kondisiLed == false) {
+      digitalWrite(pinLed, HIGH);
+      kondisiLed = true;
+      delay(5);
+    } else {
+      digitalWrite(pinLed, LOW);
+      kondisiLed = false;
+      delay(5);
+    }
+    kondisiTombolSebelumnya = kondisiTombol;
+  } else {
+    kondisiTombolSebelumnya = kondisiTombol;
+  }
 }
