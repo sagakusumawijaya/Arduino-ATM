9ca6302ac37e0941ff2aca9f633fcab2566e513e
Saga Kusuma Wijaya <45896812+sagakusumawijaya@users.noreply.github.com>
2020-04-25 08:49:00 +0700

    Update Bab_3_Code_Project_Menyalakan_LED_Menggunakan_Tombol.ino

--- BAB 3 - Input Output Digital/Project Menyalakan LED Menggunakan Tombol/Bab_3_Code_Project_Menyalakan_LED_Menggunakan_Tombol.ino
+++ BAB 3 - Input Output Digital/Project Menyalakan LED Menggunakan Tombol/Bab_3_Code_Project_Menyalakan_LED_Menggunakan_Tombol.ino
@@ -4,40 +4,25 @@
  * Project      : Tutorial Arduino ATM
  * Description  : BAB 3 - Project Menyalakan LED Menggunakan Tombol
  * Dedicated to : emtronics
+ * Youtube      : emtronics.official
  * Youtube      : https://www.youtube.com/channel/UCW4GRacd319Ml2cv-B9-uxg
  * Instagram    : https://www.instagram.com/emtronics.official/
  * Facebook     : https://web.facebook.com/emtronics.official/
  * Website      : https://sagakusumawijaya94.wordpress.com/tutorials/arduino-tutorial/
- * Github       : https://github.com/sagakusumawijaya
+ * Github       : https://github.com/sagakusumawijaya/Arduino-ATM
  * Email        : emtronics.2018@gmail.com
  **/
 
-#define pinLed 6
-#define pinTombol 2
+#define led 6
+#define tombol 2
 
 bool kondisiTombol = false;
-bool kondisiTombolSebelumnya = false;
-bool kondisiLed = false;
-
 void setup() {
-  pinMode(pinLed, OUTPUT);
-  pinMode(pinTombol, INPUT_PULLUP);  // tombol diset dengan eksternal pull-up resistor
+  pinMode(led, OUTPUT);
+  pinMode(tombol, INPUT_PULLUP);
 }
 
 void loop() {
-  kondisiTombol = digitalRead(pinTombol);
-  if (kondisiTombol == false && kondisiTombolSebelumnya != kondisiTombol) {
-    if (kondisiLed == false) {
-      digitalWrite(pinLed, HIGH);
-      kondisiLed = true;
-      delay(5);
-    } else {
-      digitalWrite(pinLed, LOW);
-      kondisiLed = false;
-      delay(5);
-    }
-    kondisiTombolSebelumnya = kondisiTombol;
-  } else {
-    kondisiTombolSebelumnya = kondisiTombol;
-  }
+  kondisiTombol = !digitalRead(tombol);
+  digitalWrite(led, kondisiTombol);
 }
